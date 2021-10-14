61c244e4e3e88640a29cddb4b6f1873c9facd5ae
Amirul Islam <amirulislamalmamun@gmail.com>
2021-10-14 16:13:44 +0600

    ready to deploy

--- main/settings.py
+++ main/settings.py
@@ -115,6 +115,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings, urls
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.urls import path
+from django.views.static import serve
 
 from . import views
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
     path('', views.home, name='home'),
     path('delete_todo/<int:pk>', views.delete_todo, name='delete-todo'),
     path('update_todo/<int:pk>', views.update_todo, name='update-todo'),
